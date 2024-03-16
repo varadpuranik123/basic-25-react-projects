330eb225c4e09c8e4b1896d9e67a175f2beeab11
vdpop <varadpuranik123@gmail.com>
2024-03-16 20:34:02 +0530

    made star rating project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Accordian from './components/Accordian'
 import RandomColor from './components/RandomColor'
+import StarRating from './components/star-rating'
 
 function App() {
   return (
     <>
       <Accordian />
       <RandomColor />
+      <StarRating />
     </>
   )
 }
---
+++ src/components/star-rating/index.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import { FaStar } from 'react-icons/fa'
+
+export default function StarRating({noOfStars = 5}){
+
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
+
+    function handleClick( getCurrentIndex ){
+        setRating(getCurrentIndex)
+    }
+
+    function handleMouseEnter( getCurrentIndex ){
+        setHover(getCurrentIndex)
+    }
+
+    function handleMouseLeave( ){
+        setHover(rating)
+    }
+
+    return(
+        <div className=" w-full h-screen flex items-center justify-center gap-x-4 bg-[#1e1e1e] ">
+        <div className="px-8 py-8 bg-black/10 border-2 border-black/5 backdrop-blur-2xl rounded-2xl flex items-center justify-center gap-x-4">
+        {            
+                [...Array(noOfStars)].map((_,index) => {
+                    index +=1
+
+                    return(
+                    <FaStar
+                    className={`${
+                        index <= (hover || rating) ? 'text-yellow-500' : 'text-gray-300/70'
+                        } cursor-pointer transition-colors duration-200 `}                        
+                        key={index}
+                        onClick={
+                            () => handleClick(index)
+                        }
+                        onMouseEnter={
+                            () => handleMouseEnter(index)
+                        }
+                        onMouseLeave={
+                            () => handleMouseLeave()
+                        }
+                        size={40}
+                    />
+                    )
+                    
+                } )
+        }
+        </div>
+        </div>
+    )
+}
